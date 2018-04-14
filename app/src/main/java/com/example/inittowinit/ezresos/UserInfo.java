76d1c9b1773849c617d462d7c04769b14a729159
charlietran714 <charlie.tran@cuw.edu>
2018-04-14 14:03:28 -0500

    Charlie

--- app/src/main/java/com/example/inittowinit/ezresos/UserInfo.java
+++ app/src/main/java/com/example/inittowinit/ezresos/UserInfo.java
@@ -2,12 +2,19 @@ package com.example.inittowinit.ezresos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class UserInfo extends AppCompatActivity {
+    private TextView studentIDTV;
+    private EditText studentIDET;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+
+
     }
 }
