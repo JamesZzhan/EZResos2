12aa991261e68bd5c1216ce304d502d29a819210
charlietran714 <charlie.tran@cuw.edu>
2018-04-14 14:13:20 -0500

    Charlie

--- app/src/main/java/com/example/inittowinit/ezresos/UserInfo.java
+++ app/src/main/java/com/example/inittowinit/ezresos/UserInfo.java
@@ -8,13 +8,22 @@ import android.widget.TextView;
 public class UserInfo extends AppCompatActivity {
     private TextView studentIDTV;
     private EditText studentIDET;
+    private TextView bookingDateTV;
+    private EditText dateET;
+    private TextView startTimeTV;
+    private EditText startTimeET;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
-
+        this.studentIDTV = (TextView)this.findViewById(R.id.studentIDTV);
+        this.studentIDET = (EditText)this.findViewById(R.id.studentIDET);
+        this.bookingDateTV = (TextView)this.findViewById(R.id.bookingDateTV);
+        this.dateET = (EditText)this.findViewById(R.id.dateET);
+        this.startTimeTV = (TextView)findViewById(R.id.startTimeTV);
+        this.startTimeET = (EditText)findViewById(R.id.studentIDET);
 
     }
 }
