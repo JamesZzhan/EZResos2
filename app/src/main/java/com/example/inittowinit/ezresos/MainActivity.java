bd63b342db39e49573ed5d3a2cb039b0ec78dbbd
alexisrolenc <alexis.rolenc@gmail.com>
2018-04-14 10:02:40 -0500

    Alexis

--- app/src/main/java/com/example/inittowinit/ezresos/MainActivity.java
+++ app/src/main/java/com/example/inittowinit/ezresos/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.inittowinit.ezresos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//lol
 public class MainActivity extends AppCompatActivity {
 
     @Override
