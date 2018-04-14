45a7c660875b4627345975e45d05a9d29c0540a5
alexisrolenc <alexis.rolenc@gmail.com>
2018-04-14 15:28:18 -0500

    Alexis

--- app/src/main/java/com/example/inittowinit/ezresos/HomePage.java
+++ app/src/main/java/com/example/inittowinit/ezresos/HomePage.java
@@ -27,21 +27,19 @@ public class HomePage extends AppCompatActivity
         Intent i = new Intent(this, UserInfo.class);
         this.startActivity(i);
     }
-    public void equipForExistResoButtonPressed (View v)
-    {
-        Intent i = new Intent(this, equipCurrentReso.class);
-        this.startActivity(i);
-    }
+
     public void cancelResoButtonPressed (View v)
     {
         Intent i = new Intent(this, manual_reservation.class);
         this.startActivity(i);
     }
+
     public void myResosButtonPressed (View v)
     {
         Intent i = new Intent(this, manual_reservation.class);
         this.startActivity(i);
     }
+
     public void profileButtonPressed (View v)
     {
         Intent i = new Intent(this, manual_reservation.class);
